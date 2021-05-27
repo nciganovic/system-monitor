720053cd57d902893cc89778f4cc359e633f9da4
nciganovic <nciganovic99@gmail.com>
2021-05-27 17:41:24 +0200

    Add Domain project

---
+++ SystemMonitorTrizma/Domain/Hardware.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Hardware
+    {
+        public string Model { get; set; }
+        public string AdditionalInfo { get; set; }
+    }
+}
